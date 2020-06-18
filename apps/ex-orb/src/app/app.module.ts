a3b8affd9e453f3f3b243bbf0e0a72b9f4893249
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-06-18 15:37:53 -0700

    SQUASHED MY FIRST ANGULAR BUGgit add . For some reason, my build needed the BidiModule from the Material library. After finding out it was missing, all I needed to do was run 'npm i @angular/cdk', which includes the bidi module. after that, the build compiled successfully

--- apps/ex-orb/src/app/app.module.ts
+++ apps/ex-orb/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxModule } from '@nrwl/nx';
 import { MaterialModule } from '../../../../libs/material/src/index';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './home/home.module';
+import { PlanetSearchModule } from './planet-search/planet-search.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    MaterialModule,
+    HomeModule,
+    PlanetSearchModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
