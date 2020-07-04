8b3c455496d3e82427677ec56f207b49e31a63db
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-07-04 11:09:43 -0700

    laying out raw template

--- apps/ex-orb/src/app/home/home.module.ts
+++ apps/ex-orb/src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MaterialModule } from '@ex-orb/material';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, MaterialModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
