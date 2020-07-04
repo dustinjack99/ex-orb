3396e5c8dcc1b7e90fe60e3efb097c269aff8d24
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-07-04 12:09:54 -0700

    deep-dived exoplanet API from NASA, constructed inital query for building visual planet map. query data and notes were put in api-key.ts

--- apps/ex-orb/src/app/planet-search/planet-search.component.ts
+++ apps/ex-orb/src/app/planet-search/planet-search.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ex-orb-planet-search',
   templateUrl: './planet-search.component.html',
-  styleUrls: ['./planet-search.component.css']
+  styleUrls: ['./planet-search.component.scss'],
 })
 export class PlanetSearchComponent implements OnInit {
+  planets = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
