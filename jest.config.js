8dae084e9f2a1c509a5676d64354a7c021989904
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-06 15:14:59 -0700

    overhauled file structure and gutted nrwl, nx, and ngrx bloat

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
+  name: 'ex-orb',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ex-orb',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
 };
