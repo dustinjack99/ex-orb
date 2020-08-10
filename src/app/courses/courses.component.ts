308a71f27e8dbc751c0f427480f37615c6295e6d
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-10 12:50:23 -0700

    got selected planet to conditionally render

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
   currentPlanet = null;
+
+  //this will eventually be the API call
+  // OR this could be your current planetary holding
+  // now that I think about it, this list should be
+  // your current planetary holdings, that way we
+  // wont have to populate all planets at once;
   planets = [
     {
       id: 1,
@@ -34,13 +40,36 @@ export class CoursesComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.resetSelectPlanet();
+  }
 
-  selectPlanet(planet) {
-    this.currentPlanet = planet;
+  resetSelectPlanet() {
+    //set real empty planet data
+    const emptyPlanet = {
+      id: null,
+      title: '',
+      description: '',
+      percentComplete: 0,
+      favorite: false,
+    };
+
+    this.currentPlanet = emptyPlanet;
+  }
+
+  cancel() {
+    this.resetSelectPlanet();
   }
 
   deletePlanet(planet) {
     console.log(planet + 'deleted');
+  }
+
+  savePlanet() {
+    console.log('planet saved');
+  }
+
+  selectPlanet(planet) {
+    this.currentPlanet = planet;
   }
 }
