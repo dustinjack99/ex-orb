6a076009798a41559ec457eafb86308ad8c7b062
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-11 10:35:22 -0700

    fought with http observable in map.service, but finally got planet data streaming into map.component. Will switch over to new git branch to keep track work seperate from genLayout branch :)

--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -1,5 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import axios from 'axios';
+import { Observable } from 'rxjs';
+
+export interface Planet {
+  pl_bmasse: number;
+  pl_edelink: string;
+  pl_hostname: string;
+  pl_name: string;
+  pl_orbper: number;
+  pl_pelink: string;
+  pl_pnum: number;
+  pl_rade: number;
+  st_glat: number;
+  st_glon: number;
+  st_metaratio: string;
+}
 
 const FULL_URL =
   'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_name,pl_pnum,pl_orbper,pl_bmasse,pl_rade,st_glon,st_glat,st_metratio, pl_edelink, pl_pelink&format=json';
@@ -12,7 +28,8 @@ export class MapService {
   constructor(private http: HttpClient) {}
 
   // to popluate all planet data at top level
-  all() {
-    return this.http.get(THIN_URL);
+  all(): Observable<Planet[]> {
+    // axios.get(FULL_URL).then((res) => res.data);
+    return this.http.get<Planet[]>(FULL_URL);
   }
 }
